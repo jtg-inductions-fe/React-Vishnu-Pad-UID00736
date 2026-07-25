a743edb0fec3e19b8e48dbb8b65474a7e2652c79
vishnu-pad-jtg <vishnu.pad@joshtechnologygroup.com>
2026-07-25 19:21:33 +0530

    [VP_A3_08]: Done with My Orders Page

--- src/types/order.types.ts
+++ src/types/order.types.ts
@@ -14,7 +14,6 @@ export interface OrderCreateResponse {
     total_amount: number;
     remaining_balance: number;
 }
-<<<<<<< HEAD
 
 export interface OrderListResponse {
     id: number;
@@ -25,5 +24,3 @@ export interface OrderListResponse {
     total_items: number;
     items_summary: string;
 }
-=======
->>>>>>> 63431eb ([VP_A3_06]: Done with cart)
